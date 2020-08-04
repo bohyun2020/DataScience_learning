00e96f9be5768e2182bd0381cd2682d7a7cf8e11
Bohyun <goodpartner77@naver.com>
2020-08-04 21:37:39 +0900

    do project for data science.

---
+++ DataScience/01_dataquest/202007_Fundamentals/02_Project_Profitable_App_Profile/Practice.py
@@ -0,0 +1,6 @@
+scores = {}
+scores['bohyun'] = 100
+scores['bohyun'] = 120
+print(scores)
+
+print(len(scores))
--- DataScience/01_dataquest/202007_Fundamentals/02_Project_Profitable_App_Profile/Project.py
+++ DataScience/01_dataquest/202007_Fundamentals/02_Project_Profitable_App_Profile/Project.py
@@ -1,5 +1,6 @@
 from csv import reader
 
+
 """
 2. Opening and Exploring the Data
 """
@@ -44,15 +45,113 @@ explore_data(ios, 1, 3, True)
     2) Detect duplicate data, and remove the duplicates.
 
 """
-"""
-print(android[10472])  # Incorrect row
-print('\n')
-print(android_header)  # header
-print('\n')
-print(android[0])      # correct row
 
-# remove the incorrect row[10472] using the del statement
+
+
+
+# 4. Removing Duplicate Entries: Part One 
+"""
+### The Google Play data set ###
+opened_file = open('googleplaystore.csv', encoding='utf-8')
+read_file = reader(opened_file)
+android = list(read_file)
+android_header = android[0]
+android = android[1:]
+
+### The App Store data set ###
+opened_file = open('AppleStore.csv', encoding='utf-8')
+read_file = reader(opened_file)
+ios = list(read_file)
+ios_header = ios[0]
+ios = ios[1:]
+
+def explore_data(dataset, start, end, rows_and_columns=False):
+    dataset_slice = dataset[start:end]    
+    for row in dataset_slice:
+        print(row)
+        print('\n') # adds a new (empty) line after each row
+
+    if rows_and_columns:
+        print('Number of rows:', len(dataset))
+        print('Number of columns:', len(dataset[0]))
+
 print(len(android))
 del android[10472]
 print(len(android))
+
+
+duplicates_apps = []
+unique_apps = []
+for app in android:
+    name = app[0]
+    if name in unique_apps:
+        duplicates_apps.append(name)
+    else: 
+        unique_apps.append(name)
+
+# print('Number of duplicate apps: ', len(duplicates_apps))
+# print('\n')
+# print(f'Examples of duplicate apps: ', duplicates_apps[:15])
+
+apps_name = ['Instagram', 'Facebook']
+
+# print('\n')
+# print('Instagram' in apps_name)
+# print('Twitter' in apps_name)
+
+reviews_max = {}
+
+for app in android:
+    name = app[0]
+    n_reviews = float(app[3])
+
+    if name in reviews_max and reviews_max[name] < n_reviews:
+        reviews_max[name] = n_reviews 
+
+    elif name not in reviews_max:
+        reviews_max[name] = n_reviews
+# android_clean = {}
+# already_added = {}
+
+# for name in reviews_max:
+#     if name not in android_clean:
+#         android_clean.append(name)
+
+android_clean = []
+already_added = []
+
+for app in android:
+    name = app[0]
+    n_reviews = float(app[3])
+    
+    if (reviews_max[name] == n_reviews) and (name not in already_added):
+        android_clean.append(app)
+        already_added.append(name) # make sure this is inside the if block
+
 """
+"""
+print(ord('A'))
+print(ord('a'))
+print(ord('元'))
+print(ord('+'))
+
+string = 'abc'
+
+for character in string:
+    print(character)
+"""
+
+
+def is_english(string):
+
+    for character in string:
+        if ord(character) > 127:
+            return False
+        else: 
+            return True
+
+# print(is_english('Instagram'))
+# print(is_english('爱奇艺PPS -《欢乐颂2》电视剧热播'))
+
+print(ord('™'))
+print(ord('😜'))
