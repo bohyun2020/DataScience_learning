9cbe495a673e92a307387e389c3ffe194835e7a9
Bohyun <goodpartner77@naver.com>
2020-08-18 16:53:48 +0900

    Learn OOP.

--- DataScience/01_dataquest/202008_Intermediate/01_Course_Mission/20200817_Object_Oriented_Python.py
+++ DataScience/01_dataquest/202008_Intermediate/01_Course_Mission/20200817_Object_Oriented_Python.py
@@ -81,5 +81,280 @@ print(type(newlist_1))
 In order to make our class do something, we need to define some methods
 Methods allow objects to perform actions.
 
+"""
+"""
+# 1) Sample
+
+my_string = "hello"   # an object of the str class
+my_list = [1, 2, 3]   # an object of the list class
+
+my_list.append(4)
+print(my_list)
+
+my_string = my_string.replace("h", "H")
+print(my_string)
+
+my_string.append("!")
+
+"""
+"""
+# 2 ) Exercise
+
+class NewList(DQ):
+    
+    def first_method():
+        return "This is my first method."
+    
+newlist = NewList()
+
+"""
+"""
+# 6. Understanding 'self'
+
+class NewList():
+
+    def first_method(self):
+        print("hello")
+
+instance = NewList()
+instance.first_method()
+"""
+"""
+# TypeError: first_method() takes 0 positional arguments but 1 was given        # 이해하는 거 중요합니다. 
+
+* It says that one argument was given to first_method(), but when we called 
+the method we didn't provide any arguments. It seems like there is a "phantom" 
+argument being inserted somewhere. 
 
 
+"""
+"""
+# 1) Sample
+# create a str object
+s = "MY STRING"
+
+# call `str.title() directly
+# instead of `s.title()`
+result = str.title(s)
+print(result)
+"""
+"""
+# 2) Sample
+class MyClass():
+    def print_self(self):
+        print(self)
+
+mc = MyClass()
+mc.print_self()
+"""
+"""
+# 3) Exercise
+
+class NewList():
+
+    def first_method(self):
+        return "This is my first method"
+
+newlist = NewList()
+result = newlist.first_method()
+"""
+"""
+# 7. Creating a Method That Accepts an Argument
+
+"""
+"""
+1) Sample
+class MyClass():
+    def return_string(self, string):
+        return string
+
+mc = MyClass()
+result = mc.return_string("Hey there!")
+print(result)
+"""
+"""
+2) Exercise
+class NewList(DQ):
+    
+    def return_list(self, input_list):
+        return input_list
+  
+newlist = NewList()
+result = newlist.return_list([1, 2, 3])
+"""
+"""
+# 8. Attributes and the Init Method
+
+The power of objects is in their ability to store data, 
+and data is stored inside objects using attributes.
+
+Relating back to our Tesla metaphor, an object of the Tesla "class" has 
+attributes like their color, battery, and motor.
+
+You can think of attributes like special variables that belong to a particular class.
+
+We define what is done with any arguments provided at instantiation using the init method.
+The init method — also called a constructor 
+
+The init method's most common usage is to store data as an attribute:
+
+"""
+"""
+# 1) Sample
+class MyClass():
+    def __init__(self, string):
+        print(string)
+
+mc = MyClass("Holla")
+"""
+"""
+# 2) Sample
+
+class MyClass():
+    def __init__(self, string):
+        self.my_attribute = string  # string = "Hola!"
+
+mc = MyClass("Hola!") # now we have stored "Hola" in the attribute my_attribute inside our object.
+print(mc.my_attribute) # Like methods, attributes are accessed using dot notation, 
+                       #    but attributes don't have parentheses like methods do. 
+
+"""
+"""
+Let's take a moment to summarize what we've learned so far:
+
+1) The power of objects is in their ability to store data.
+2) Data is stored as attributes inside objects.
+3) We access attributes using dot notation.
+    To give attributes values when we instantiate objects, 
+4) we pass them as arguments to a special method called __init__(), 
+    which runs when we instantiate an object.
+"""
+"""
+# 3) Exercise
+class NewList(DQ):
+
+
+    def __init__(self, initial_state):
+        self.data = initial_state
+        
+my_list = NewList([1, 2, 3, 4, 5])
+print(my_list.data)
+"""
+"""
+# 9. Creating an Append Method
+
+"""
+"""
+# 1) Sample
+my_list = [1, 2, 3, 4]
+my_list.append(5)
+print(my_list)
+
+my_list = [1, 2, 3]
+new_item = 4
+new_item_list = [new_item]
+my_list = my_list + new_item_list
+print(my_list)
+"""
+"""
+class NewList():
+   
+    def __init__(self, initial_state):
+        self.data = initial_state
+
+    def append(self, new_item):
+        self.data += [new_item]
+
+my_list = NewList([1, 2, 3, 4, 5])
+print(my_list.data)
+
+my_list.append(6)
+print(my_list.data)
+"""
+"""
+# 10. Creating and Updating an Attribute
+
+class NewList():
+    
+    def __init__(self, initial_state):
+        self.data = initial_state
+
+        # We added code below this comment
+        length = 0 
+        for item in self.data:
+            length += 1
+        self.length = length
+        # We added code above this comment
+    
+    def append(self, new_item):
+       
+        self.data = self.data + [new_item]
+
+my_list = NewList([1, 2, 3])
+print(my_list.length)
+
+my_list.append(4)
+print(my_list.length)
+"""
+"""
+
+Rather than writing the code out twice, we can add a helper method, 
+which calculates the length, and just call that method in the appropriate places.
+
+"""
+"""
+# 1) Sample
+class MyBankBalance():
+    # An object that tracks a bank account balance
+
+    def __init__(self, inital_balance):
+        self.balance = inital_balance
+        self.cacl_string()
+
+    def cacl_string(self):
+        # A helper method to update self.string
+        string_balance = "${:,.2f}".format(self.balance)
+        self.string = string_balance
+
+    def add_value(self, value):
+        # Add value to the bank balance 
+        self.balance += value
+        self.cacl_string()
+
+mbb = MyBankBalance(3.50)
+print(mbb.string)
+
+mbb.add_value(5000)
+print(mbb.string)
+"""
+
+class NewList():
+    """
+    A Python list with some extras!
+    """
+    def __init__(self, initial_state):
+        self.data = initial_state
+        self.calc_length()
+    
+    def calc_length(self):
+        """
+        A helper function to calculate the .length
+        attribute.
+        """
+        length = 0
+        for item in self.data:
+            length += 1
+        self.length = length
+    
+    def append(self, new_item):
+        """
+        Append `new_item` to the NewList
+        """
+        self.data = self.data + [new_item]
+        self.calc_length()
+
+fibonacci = NewList([1, 1, 2, 3, 5])
+print(fibonacci.length)
+
+fibonacci.append(8)
+print(fibonacci.length)
